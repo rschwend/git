bdea8b848b24131aa90d73aeeb7976d2fb730c2a
rschwend <raphael.schwendener@outlook.com>
2023-08-18 01:03:41 +0200

    example addes

--- Trend-Micro---ExclusionList-Converter.py
+++ Trend-Micro---ExclusionList-Converter.py
@@ -3,6 +3,8 @@
 import re
 
 excl = r"C:\Users\rap\example\Downloads\file.exe|d:\app\my.exe|%windir%\app\azzi.exe"
+exa = r"C:\Users\rap\example\Downloads\file.exe|d:\app\my.exe|%windir%\app\azzi.exe"
+print ("INPUT Example: " + exa)
 #excl = str(input())
 x = re.split(r'[|]', excl)
 
